7e413dbd62a554087acaf39692e5b63a0486f2d9
Reginald Chand <reginald-chand@outlook.com>
2024-07-02 13:25:34 +1200

    Add Footer

--- app/components/layouts/_app.tsx
+++ app/components/layouts/_app.tsx
@@ -1,7 +1,13 @@
 import { ReactElement, ReactNode } from "react";
+import { Footer } from "../ui/footer";
 
 export const GlobalLayout = ({
   children,
 }: Readonly<{ children: ReactNode }>): ReactElement => {
-  return <>{children}</>;
+  return (
+    <>
+      {children}
+      <Footer />
+    </>
+  );
 };
