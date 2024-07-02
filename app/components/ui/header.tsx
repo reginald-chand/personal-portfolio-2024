a88fdec05852e83c7b9635f3a1f8c42ca405e43b
Reginald Chand <reginald-chand@outlook.com>
2024-07-03 01:38:56 +1200

    Create header.tsx

---
+++ app/components/ui/header.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { ReactElement, memo, useState } from "react";
+import { Logo } from "./logo";
+import { Hamburger } from "./hamburger";
+import { HeaderNavigation } from "./header-navigation";
+import { HeaderNavLinks } from "../data/header-nav-links";
+
+export const Header = memo((): ReactElement => {
+  const [navigationDrawer, setNavigationDrawer] = useState<boolean>(false);
+
+  const handleNavigationDrawer = () => {
+    setNavigationDrawer((prev) => !prev);
+  };
+
+  return (
+    <header className="w-full h-20 p-5 sticky top-0 left-0 flex items-center justify-between border-b-2 border-gray-900 border-dashed bg-gray-950">
+      <Logo brandName="Reginald Chand" brandSlogan="Full Stack Engineer" />
+      <Hamburger
+        hamburgerFunction={handleNavigationDrawer}
+        hamburgerAccessibilityText="Open Navigation Menu"
+      />
+      <HeaderNavigation
+        navigationDrawer={navigationDrawer}
+        handleNavigationDrawer={handleNavigationDrawer}
+        headerNavigationLinks={HeaderNavLinks}
+      />
+    </header>
+  );
+});
+
+Header.displayName = "Header";
