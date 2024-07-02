ee5d80e585716ff31297c4a0296f084b0154c3d7
Reginald Chand <reginald-chand@outlook.com>
2024-07-03 01:39:24 +1200

    Add Header

--- app/components/layouts/_app.tsx
+++ app/components/layouts/_app.tsx
@@ -1,11 +1,13 @@
 import { ReactElement, ReactNode } from "react";
 import { Footer } from "../ui/footer";
+import { Header } from "../ui/header";
 
 export const GlobalLayout = ({
   children,
 }: Readonly<{ children: ReactNode }>): ReactElement => {
   return (
     <>
+      <Header />
       {children}
       <Footer />
     </>
