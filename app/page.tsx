02eba2bd0ffa1d1538418fae7f5023822c7f7d12
Reginald Chand <reginald-chand@outlook.com>
2024-07-04 01:07:00 +1200

    Add FeaturedProjectsSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { ReactElement } from "react";
 import { GlobalLayout } from "./components/layouts/_app";
 import { HeroSection } from "./components/ui/hero_section/hero-section";
+import { FeaturedProjectsSection } from "./components/ui/featured_projects_section/featured-projects-section";
 
 export default function Home(): ReactElement {
   return (
     <GlobalLayout>
       <main className="w-full min-h-screen">
         <HeroSection />
+        <FeaturedProjectsSection />
       </main>
     </GlobalLayout>
   );
