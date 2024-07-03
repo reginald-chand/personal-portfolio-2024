920cc36bd4fd192db69de5543a3e56fdab0ea84c
Reginald Chand <reginald-chand@outlook.com>
2024-07-03 22:09:16 +1200

    Add HeroSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { ReactElement } from "react";
 import { GlobalLayout } from "./components/layouts/_app";
+import { HeroSection } from "./components/ui/hero_section/hero-section";
 
 export default function Home(): ReactElement {
   return (
     <GlobalLayout>
-      <main className="w-full min-h-screen"></main>
+      <main className="w-full min-h-screen">
+        <HeroSection />
+      </main>
     </GlobalLayout>
   );
 }
