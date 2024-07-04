1d6b98147639765dc1ec0a3f9378c3cdba7e4d49
Reginald Chand <reginald-chand@outlook.com>
2024-07-05 05:27:50 +1200

    Refactor code

--- app/projects/fullstack/page.tsx
+++ app/projects/fullstack/page.tsx
@@ -1,12 +1,33 @@
-import { GlobalLayout } from "@/app/components/layouts/_app";
-import { ProjectsSection } from "@/app/components/ui/page/projects/projects-section";
-import { ReactElement } from "react";
+"use client";
 
-export default function Home(): ReactElement {
+import { ReactElement, useState } from "react";
+import { GlobalLayout } from "@/app/components/layouts/_app";
+import { Projects } from "@/app/components/ui/page/projects/projects";
+import { useProjectList } from "@/app/components/hooks/use-project-list";
+import { ProjectsNavigation } from "@/app/components/ui/page/projects/projects-navigation";
+import { ProjectsSearch } from "@/app/components/ui/page/projects/projects-search";
+import { FullStackProjectsData } from "@/app/components/data/fullstack-projects-data";
+
+export default function FullStackProjects(): ReactElement {
+  const [searchPrompt, setSearchPrompt] = useState<string>();
+
+  const handleSearchPrompt = (event: any) => {
+    setSearchPrompt(event.target.value);
+  };
+
+  const [newProjectList] = useProjectList({
+    searchPrompt: searchPrompt,
+    projectList: FullStackProjectsData,
+  });
+
   return (
     <GlobalLayout>
       <main className="w-full min-h-screen">
-        <ProjectsSection />
+        <section className="p-5 grid grid-rows-[80px,1fr]  md:grid-cols-[300px,auto] gap-5">
+          <ProjectsSearch handleSearchPrompt={handleSearchPrompt} />
+          <ProjectsNavigation />
+          <Projects projectList={newProjectList} />
+        </section>
       </main>
     </GlobalLayout>
   );
