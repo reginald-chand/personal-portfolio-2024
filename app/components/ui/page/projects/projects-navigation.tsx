30d134eed28cb8805fa010d9fe1b32119b044f3f
Reginald Chand <reginald-chand@outlook.com>
2024-07-05 05:25:02 +1200

    Refactor code

--- app/components/ui/page/projects/projects-navigation.tsx
+++ app/components/ui/page/projects/projects-navigation.tsx
@@ -1,33 +1,26 @@
+import Link from "next/link";
 import { ReactElement, memo } from "react";
 
 export const ProjectsNavigation = memo((): ReactElement => {
-  const openBackendProjects = () => {
-    if (!window.location.href.includes("projects/backend")) {
-      window.location.href = "/projects/backend";
-    }
-  };
-
-  const openFullStackProjects = () => {
-    if (!window.location.href.includes("projects/fullstack")) {
-      window.location.href = "/projects/fullstack";
-    }
-  };
-
   return (
     <div className="max-h-[190px] p-5 border-2 border-dashed border-gray-900 rounded-md">
       <ul>
-        <li
-          onClick={openBackendProjects}
-          className="mb-5 p-5 text-sm font-bold cursor-pointer uppercase border-2 border-dashed border-brand-primary rounded-md hover:border-gray-900 hover:text-brand-primary active:scale-95 transition-all duration-300 ease-linear"
-        >
-          Backend
+        <li>
+          <Link
+            href="/projects/backend"
+            className="mb-5 p-5 block text-sm font-bold uppercase border-2 border-dashed border-brand-primary rounded-md hover:border-gray-900 hover:text-brand-primary active:scale-95 transition-all duration-300 ease-linear"
+          >
+            Backend
+          </Link>
         </li>
 
-        <li
-          onClick={openFullStackProjects}
-          className="p-5 text-sm font-bold cursor-pointer uppercase border-2 border-dashed border-brand-primary rounded-md hover:border-gray-900 hover:text-brand-primary active:scale-95 transition-all duration-300 ease-linear"
-        >
-          Full Stack
+        <li>
+          <Link
+            href="/projects/fullstack"
+            className="mb-5 p-5 block text-sm font-bold uppercase border-2 border-dashed border-brand-primary rounded-md hover:border-gray-900 hover:text-brand-primary active:scale-95 transition-all duration-300 ease-linear"
+          >
+            Full Stack
+          </Link>
         </li>
       </ul>
     </div>
