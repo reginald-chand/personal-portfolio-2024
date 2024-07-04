08aa4259c10a33254ad1faa121592d11c2c34d01
Reginald Chand <reginald-chand@outlook.com>
2024-07-05 04:41:53 +1200

    Create projects-section.tsx

---
+++ app/components/ui/page/projects/projects-section.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { ReactElement, memo, useState } from "react";
+import { ProjectsNavigation } from "./projects-navigation";
+import { Projects } from "./projects";
+import { useProjectList } from "../../../hooks/use-project-list";
+import { ProjectsSearch } from "./projects-search";
+import { FullStackProjectsData } from "@/app/components/data/fullstack-projects-data";
+import { FeaturedProjectsSectionProjectsData } from "@/app/components/data/featured-projects-section-projects-data";
+
+export const ProjectsSection = memo((): ReactElement => {
+  const [searchPrompt, setSearchPrompt] = useState<string>();
+
+  const backendProjectsUrl = "projects/backend";
+  const fullStackProjectsUrl = "projects/fullstack";
+
+  const handleSearchPrompt = (event: any) => {
+    setSearchPrompt(event.target.value);
+  };
+
+  const [backendNewProjectList] = useProjectList({
+    searchPrompt: searchPrompt,
+    projectList: FeaturedProjectsSectionProjectsData,
+  });
+
+  const [fullStackNewProjectList] = useProjectList({
+    searchPrompt: searchPrompt,
+    projectList: FullStackProjectsData,
+  });
+
+  return (
+    <section className="p-5 grid grid-rows-[80px,1fr]  md:grid-cols-[300px,auto] gap-5">
+      <ProjectsSearch handleSearchPrompt={handleSearchPrompt} />
+      <ProjectsNavigation />
+      <Projects
+        hideView={
+          window.location.href.includes(backendProjectsUrl) ? false : true
+        }
+        projectList={backendNewProjectList}
+      />
+      <Projects
+        hideView={
+          window.location.href.includes(fullStackProjectsUrl) ? false : true
+        }
+        projectList={fullStackNewProjectList}
+      />
+    </section>
+  );
+});
+
+ProjectsSection.displayName = "Projects Section";
