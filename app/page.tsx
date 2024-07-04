66e1b33d8244bcb5e29ff425f62489003d933b79
Reginald Chand <reginald-chand@outlook.com>
2024-07-04 20:41:00 +1200

    Add UpComingProjectsSection

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { ReactElement } from "react";
 import { GlobalLayout } from "./components/layouts/_app";
 import { HeroSection } from "./components/ui/hero_section/hero-section";
 import { FeaturedProjectsSection } from "./components/ui/featured_projects_section/featured-projects-section";
+import { UpComingProjectsSection } from "./components/ui/upcoming_projects_section/upcoming-projects-section";
 
 export default function Home(): ReactElement {
   return (
@@ -9,6 +10,7 @@ export default function Home(): ReactElement {
       <main className="w-full min-h-screen">
         <HeroSection />
         <FeaturedProjectsSection />
+        <UpComingProjectsSection />
       </main>
     </GlobalLayout>
   );
