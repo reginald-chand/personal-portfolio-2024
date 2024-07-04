9150e529cc871bf3522a9d911ce08450013658ac
Reginald Chand <reginald-chand@outlook.com>
2024-07-05 04:40:36 +1200

    Create fullstack-projects-data.tsx

---
+++ app/components/data/fullstack-projects-data.tsx
@@ -0,0 +1,14 @@
+type FullStackProjectsType = {
+  title: string;
+  link: string;
+  description: string;
+};
+
+export const FullStackProjectsData: Readonly<FullStackProjectsType[]> = [
+  {
+    title: "Personal portfolio 2024",
+    link: "https://github.com/reginald-chand/personal-portfolio-2024",
+    description:
+      "I am passionate about crafting efficient and scalable software solutions, focusing on backend development, microservices architecture, and ensuring high-performance standards. My projects emphasize security, scalability, and seamless integration within complex systems.",
+  },
+];
