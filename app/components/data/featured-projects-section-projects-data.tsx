6a31424c03bccd7dcc6e84e26e9398d410602fa5
Reginald Chand <reginald-chand@outlook.com>
2024-07-05 13:09:06 +1200

    Update types

--- app/components/data/featured-projects-section-projects-data.tsx
+++ app/components/data/featured-projects-section-projects-data.tsx
@@ -1,65 +1,60 @@
-type FeaturedProjectsSectionProjectsType = {
-  title: string;
-  link: string;
-  description: string;
-};
+import { ProjectsDataType } from "../types/projects-data-type";
 
-export const FeaturedProjectsSectionProjectsData: Readonly<
-  FeaturedProjectsSectionProjectsType[]
-> = [
-  {
-    title: "Identity management system backend",
-    link: "https://github.com/reginald-chand/identity-management-system-backend",
-    description:
-      "A secure Identity Management System (IMS) backend microservice, designed for robust user management with stringent security measures, ideal for seamless integration within microservices architecture.",
-  },
+export const FeaturedProjectsSectionProjectsData: Readonly<ProjectsDataType[]> =
+  [
+    {
+      title: "Identity management system backend",
+      link: "https://github.com/reginald-chand/identity-management-system-backend",
+      description:
+        "A secure Identity Management System (IMS) backend microservice, designed for robust user management with stringent security measures, ideal for seamless integration within microservices architecture.",
+    },
 
-  {
-    title: "Post management system backend",
-    link: "https://github.com/reginald-chand/post-management-system-backend",
-    description:
-      "An agile Post Management System (PMS) backend microservice, designed for efficient content handling and seamless integration in microservices architecture, ensuring robust performance and security.",
-  },
+    {
+      title: "Post management system backend",
+      link: "https://github.com/reginald-chand/post-management-system-backend",
+      description:
+        "An agile Post Management System (PMS) backend microservice, designed for efficient content handling and seamless integration in microservices architecture, ensuring robust performance and security.",
+    },
 
-  {
-    title: "Social relationship management system backend",
-    link: "https://github.com/reginald-chand/social-relationship-management-system-backend",
-    description:
-      "Social Relationship Management System (SRMS) backend microservice optimizes social connections, ensuring efficient content handling, robust performance, and secure integration for applications.",
-  },
+    {
+      title: "Social relationship management system backend",
+      link: "https://github.com/reginald-chand/social-relationship-management-system-backend",
+      description:
+        "Social Relationship Management System (SRMS) backend microservice optimizes social connections, ensuring efficient content handling, robust performance, and secure integration for applications.",
+    },
 
-  {
-    title: "Feed management system backend",
-    link: "https://github.com/reginald-chand/feed-management-system-backend",
-    description:
-      "An agile Feed Management System (FMS) backend microservice, optimizing content delivery and integration in microservices, prioritizing performance and security.",
-  },
+    {
+      title: "Feed management system backend",
+      link: "https://github.com/reginald-chand/feed-management-system-backend",
+      description:
+        "An agile Feed Management System (FMS) backend microservice, optimizing content delivery and integration in microservices, prioritizing performance and security.",
+    },
 
-  {
-    title: "Like management system backend",
-    link: "https://github.com/reginald-chand/like-management-system-backend",
-    description:
-      "Enhance user engagement with my Like Management System (LMS) powered by agile backend microservices, ensuring optimized performance and robust security measures.",
-  },
+    {
+      title: "Like management system backend",
+      link: "https://github.com/reginald-chand/like-management-system-backend",
+      description:
+        "Enhance user engagement with my Like Management System (LMS) powered by agile backend microservices, ensuring optimized performance and robust security measures.",
+    },
 
-  {
-    title: "Comment management system backend",
-    link: "https://github.com/reginald-chand/comment-management-system-backend",
-    description:
-      "Transform your platform with my Comment Management System (CMS) backend microservice, ensuring enhanced performance and security features.",
-  },
+    {
+      title: "Comment management system backend",
+      link: "https://github.com/reginald-chand/comment-management-system-backend",
+      description:
+        "Transform your platform with my Comment Management System (CMS) backend microservice, ensuring enhanced performance and security features.",
+    },
 
-  {
-    title: "Share post management system backend",
-    link: "https://github.com/reginald-chand/share-post-management-system-backend",
-    description:
-      "Optimize your platform with my Share Posts Management System (SPMS) backend microservice, ensuring efficient post sharing and advanced security features.",
-  },
+    {
+      title: "Share post management system backend",
+      link: "https://github.com/reginald-chand/share-post-management-system-backend",
+      description:
+        "Optimize your platform with my Share Posts Management System (SPMS) backend microservice, ensuring efficient post sharing and advanced security features.",
+    },
 
-  {
-    title: "User form submission backend",
-    link: "https://github.com/reginald-chand/user-form-submission-backend",
-    description:
-      "Optimize user form submissions with my user form submission backend microservice (UFSB), ensuring efficient data handling and advanced security features.",
-  },
-];
+    {
+      title: "User form submission backend",
+      link: "https://github.com/reginald-chand/user-form-submission-backend",
+      description:
+        "Optimize user form submissions with my user form submission backend microservice (UFSB), ensuring efficient data handling and advanced security features.",
+    },
+  ];
