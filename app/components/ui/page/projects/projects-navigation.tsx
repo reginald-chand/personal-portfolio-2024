ac28642a03edd458ab1b26d9773b165315a12564
Reginald Chand <reginald-chand@outlook.com>
2024-07-05 19:16:08 +1200

    Add active pathName indicator using usePathname()

--- app/components/ui/page/projects/projects-navigation.tsx
+++ app/components/ui/page/projects/projects-navigation.tsx
@@ -1,14 +1,24 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { ReactElement, memo } from "react";
 
 export const ProjectsNavigation = memo((): ReactElement => {
+  const pathName = usePathname();
+
+  const backendProjectsPathName = "/projects/backend";
+  const fullStackProjectsPathName = "/projects/fullstack";
+
   return (
     <div className="max-h-[190px] p-5 border-2 border-dashed border-gray-900 rounded-md">
       <ul>
         <li>
           <Link
             href="/projects/backend"
-            className="mb-5 p-5 block text-sm font-bold uppercase border-2 border-dashed border-brand-primary rounded-md hover:border-gray-900 hover:text-brand-primary active:scale-95 transition-all duration-300 ease-linear"
+            className={`mb-5 p-5 block text-sm ${
+              pathName === backendProjectsPathName
+                ? "border-brand-primary"
+                : "border-gray-900 "
+            } font-bold uppercase border-2 border-dashed rounded-md hover:border-brand-primary active:scale-95 transition-all duration-300 ease-linear`}
           >
             Backend
           </Link>
@@ -17,7 +27,11 @@ export const ProjectsNavigation = memo((): ReactElement => {
         <li>
           <Link
             href="/projects/fullstack"
-            className="mb-5 p-5 block text-sm font-bold uppercase border-2 border-dashed border-brand-primary rounded-md hover:border-gray-900 hover:text-brand-primary active:scale-95 transition-all duration-300 ease-linear"
+            className={`mb-5 p-5 block text-sm ${
+              pathName === fullStackProjectsPathName
+                ? "border-brand-primary"
+                : "border-gray-900 "
+            } font-bold uppercase border-2 border-dashed rounded-md hover:border-brand-primary active:scale-95 transition-all duration-300 ease-linear`}
           >
             Full Stack
           </Link>
