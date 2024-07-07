17bdcab7ebdc35e7cf9dee5b9c0ef69677a3da75
Reginald Chand <reginald-chand@outlook.com>
2024-07-08 00:47:59 +1200

    Create page.tsx

---
+++ app/admin/page.tsx
@@ -0,0 +1,14 @@
+import { ReactElement } from "react";
+import { GlobalLayout } from "../components/layouts/_app";
+
+export default function Admin(): ReactElement {
+  return (
+    <GlobalLayout>
+      <main className="w-full 2xl:max-w-[1920px] 2xl:mx-auto min-h-screen">
+        <h1 className="m-5 p-5 border-2 border-dashed border-gray-900 rounded-md">
+          Coming Soon
+        </h1>
+      </main>
+    </GlobalLayout>
+  );
+}
