b1ecc56786ff2defe0c50db4fedb8c024bfe2b17
Reginald Chand <reginald-chand@outlook.com>
2024-07-07 17:10:35 +1200

    Add AboutWhyHireMe

--- app/components/ui/page/about/about-section.tsx
+++ app/components/ui/page/about/about-section.tsx
@@ -2,12 +2,14 @@ import { ReactElement, memo } from "react";
 import { AboutHeroSection } from "./about-hero-section";
 import { AboutSkillsSection } from "./about-skills-section";
 import { AboutContactSection } from "./about-contact-section";
+import { AboutWhyHireMe } from "./about-why-hire-me-section";
 
 export const AboutSection = memo((): ReactElement => {
   return (
     <>
       <AboutHeroSection />
       <AboutSkillsSection />
+      <AboutWhyHireMe />
       <AboutContactSection />
     </>
   );
