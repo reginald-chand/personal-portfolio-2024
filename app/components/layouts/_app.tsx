a09c64138b06d1950d6c976e0f38311540389c57
Reginald Chand <reginald-chand@outlook.com>
2024-07-08 13:44:34 +1200

    Add Scene

--- app/components/layouts/_app.tsx
+++ app/components/layouts/_app.tsx
@@ -2,12 +2,14 @@ import { ReactElement, ReactNode } from "react";
 import { Footer } from "../ui/footer";
 import { Header } from "../ui/header";
 import NextTopLoader from "nextjs-toploader";
+import { Scene } from "./scene";
 
 export const GlobalLayout = ({
   children,
 }: Readonly<{ children: ReactNode }>): ReactElement => {
   return (
     <>
+      <Scene />
       <Header />
       <NextTopLoader color="#3b82f6" />
       {children}
